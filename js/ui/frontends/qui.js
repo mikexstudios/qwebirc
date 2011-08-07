ead9984c46d5e34e8c669cd15d01d3e7bcf748f7
Michael Huynh <mike.huynh@gmail.com>
2011-08-07 18:22:07 -0400

    Commented out dropdown hint.

--- js/ui/frontends/qui.js
+++ js/ui/frontends/qui.js
@@ -53,7 +53,7 @@ qwebirc.ui.QUI = new Class({
     this.reflow.delay(100); /* Konqueror fix */
     
     /* HACK, in Chrome this should work immediately but doesn't */
-    this.__createDropdownHint.delay(100, this);
+    //this.__createDropdownHint.delay(100, this);
   },
   __createDropdownMenu: function() {
     var dropdownMenu = new Element("span");
